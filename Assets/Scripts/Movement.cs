1f56b13cf459d8e64de6b689a61d13dba94b7fe0
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-11-12 23:16:14 +0300

    make a variable changes for animation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
   private Rigidbody2D rigidbody2d;
   private Vector2 movement;
   [SerializeField] private float movementSpeed;
+  [SerializeField] Animator animator;
+
 
   private void Awake()
   {
@@ -18,10 +20,14 @@ public class Movement : MonoBehaviour
   {
     movement.x = Input.GetAxis("Horizontal");
     movement.y = Input.GetAxis("Vertical");
+
+    animator.SetFloat("Horizontal", movement.x);
+    animator.SetFloat("Vertical", movement.y);
+    animator.SetFloat("Speed", movement.sqrMagnitude);
   }
 
   void FixedUpdate()
   {
-    rigidbody2d.MovePosition(rigidbody2d.position + movement * movementSpeed * Time.fixedDeltaTime);
+    rigidbody2d.MovePosition(rigidbody2d.position + (movement * movementSpeed * Time.fixedDeltaTime));
   }
 }
