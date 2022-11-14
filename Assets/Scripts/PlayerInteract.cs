8bf2c4c4ac9ea297f0df84851d6012234997b54b
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-11-15 01:00:22 +0300

    fix player position on tilemap interact

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -9,10 +9,8 @@ public class PlayerInteract : MonoBehaviour
   {
     if (Input.GetKeyDown(KeyCode.Space))
     {
-      int x = Mathf.CeilToInt(Mathf.Abs(transform.position.x)) * (int)Mathf.Sign(transform.position.x);
-      int y = Mathf.CeilToInt(Mathf.Abs(transform.position.y)) * (int)Mathf.Sign(transform.position.y);
-
-      Debug.Log($"{x},{y}; {transform.position.x}, {transform.position.y}");
+      int x = Mathf.FloorToInt(transform.position.x);
+      int y = Mathf.FloorToInt(transform.position.y);
 
       Vector3Int position = new Vector3Int(x, y, 0);
       TileManager tl = GameManager.instance.tileManager;
