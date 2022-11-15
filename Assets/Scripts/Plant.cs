b2e0aff6736a20f17fe00cedeb6849852198794e
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-11-16 02:07:40 +0300

    created class Plant

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Plant
+{
+  public int posX, posY;
+  public string name;
+
+  public Plant(int posX, int posY, string name)
+  {
+    this.posX = posX;
+    this.posY = posY;
+    this.name = name;
+  }
+
+
+}
