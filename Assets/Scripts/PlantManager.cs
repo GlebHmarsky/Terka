8e5641287a2a61132dfc4e730a351c37a88406af
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-11-17 19:21:08 +0300

    add plowed component and give it to Plowed prefab

--- Assets/Scripts/PlantManager.cs
+++ Assets/Scripts/PlantManager.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PlantManager : MonoBehaviour
 {
+  public List<GameObject> plants;
+  public List<GameObject> plowed;
 
-  // TODO: do not use object Plant, use GameObject WITH Plant IN IT
-  public List<Plant> plants;
-  public GameObject prefab;
-
+  public GameObject plantPrefab;
+  public GameObject plowedPrefab;
 
 
   void Start()
   {
-    plants = new List<Plant>();
+    plants = new List<GameObject>();
+    plowed = new List<GameObject>();
   }
 
   public void CreatePlant(int posX, int posY, string name)
   {
-    plants.Add(new Plant(posX, posY, name));
+    GameObject plantGameObject = Instantiate(plantPrefab, new Vector3(posX + 0.5f, posY + 0.5f, 0), Quaternion.identity);
+    plants.Add(plantGameObject);
 
-    Instantiate(prefab, new Vector3(posX + 0.5f, posY + 0.5f, 0), Quaternion.identity);
+    Plant plant = plantGameObject.GetComponent<Plant>();
+    plant.plantName = name;
+    plant.posX = posX;
+    plant.posY = posY;
+
+  }
+  public void CreatePlowed(int posX, int posY, string name)
+  {
+    GameObject plowedGameObject = Instantiate(plowedPrefab, new Vector3(posX + 0.5f, posY + 0.5f, 0), Quaternion.identity);
+    this.plowed.Add(plowedGameObject);
+
+    Plowed plowed = plowedGameObject.GetComponent<Plowed>();
+    plowed.posX = posX;
+    plowed.posY = posY;
+
   }
 }
--- Assets/Scripts/Plowed.cs
+++ Assets/Scripts/Plowed.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plant : MonoBehaviour
+public class Plowed : MonoBehaviour
 {
   public int posX, posY;
-  public string plantName;
 
-  public Plant(int posX, int posY, string name)
+  public Plowed(int posX, int posY, string name)
   {
     this.posX = posX;
     this.posY = posY;
-    this.plantName = name;
   }
-
 
 }
