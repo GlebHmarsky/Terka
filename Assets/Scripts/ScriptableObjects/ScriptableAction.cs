a429a6ff384d9b9d7882dc2f3f4346eb0788d9d2
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-12-20 17:07:22 +0300

    add light to the scene

--- Assets/Scripts/ScriptableObjects/ScriptableAction.cs
+++ Assets/Scripts/ScriptableObjects/ScriptableAction.cs
@@ -8,8 +8,5 @@ public abstract class ScriptableAction : ScriptableObject
   /// <summary>
   /// Perform some action
   /// </summary>
-  /// <param name="position"></param>
-  /// <param name="inventoryManager"></param>
-  /// <returns>False if there some problem and action work not as planed. True otherwise</returns>
   public abstract void PerformAction(Vector2Int position, InventoryManager inventoryManager);
 }
