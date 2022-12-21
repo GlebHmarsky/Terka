b0d21e4eb35bd6f1d58ed685fd9903c42543e87a
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-12-22 00:17:59 +0300

    make a moveable select area on mouse move

---
+++ Assets/Scripts/MouseMovementSelect.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovementSelect : MonoBehaviour
+{
+  public Vector3 position;
+  void Update()
+  {
+    OnMouseMovement();
+  }
+
+  void OnMouseMovement()
+  {
+    Vector3 screenPosition = Input.mousePosition;
+    Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+    position = new Vector3(Mathf.FloorToInt(worldPosition.x) + 0.5f, Mathf.FloorToInt(worldPosition.y) + 0.5f, 0);
+    transform.position = position;
+  }
+}
